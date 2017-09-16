6e4773ec37fe7c1268d8b88996037ea84d42d59d
Alex Ware <alex.ware@gmail.com>
2017-09-16 22:22:02 +0100

    Be more careful checking if a function call is to console.log

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ export default function ({ types: t }) {
   return {
     visitor: {
       CallExpression: (path, state) => {
-        if (path.node.callee.object.name === 'console') {
+        if (path.node && path.node.callee && path.node.callee.object && path.node.callee.object.name && path.node.callee.property.name && path.node.callee.object.name === 'console') {
           if (['log', 'warn', 'debug', 'info', 'error'].includes(path.node.callee.property.name)) {
             let absoluteFilename = state.file.opts.filename
             let filename = absoluteFilename.slice(absoluteFilename.lastIndexOf('/') + 1)
